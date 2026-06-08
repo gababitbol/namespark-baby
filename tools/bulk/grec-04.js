474706d4a69c70f0e3df05b6faf679e861a499ee
Gabriel Abitbol <gabrielabitbol@MacBook-Air.local>
2026-06-08 14:49:50 +0300

    Cap 3000 dépassé → 3002 prénoms (grec célestes)

---
+++ tools/bulk/grec-04.js
@@ -0,0 +1,24 @@
+module.exports = { origin: "grec", data: `
+Apollon;b;Dieu du soleil et des arts;God of sun and arts
+Atlas;b;Qui porte le ciel;Bearer of the heavens
+Cyrus;b;Soleil, seigneur;Sun, lord
+Helios;b;Soleil;Sun
+Castor;b;Brillant, castor;Shining
+Pollux;b;Très doux;Very sweet
+Theron;b;Chasseur;Hunter
+Zephyrin;b;Vent doux d'ouest;Gentle west wind
+Asteria;g;Étoilée;Of the stars
+Callisto;g;La plus belle;Most beautiful
+Cassiopée;g;Au regard splendide;Splendid face
+Cybèle;g;Déesse mère;Mother goddess
+Elektra;g;Ambre, rayonnante;Amber, shining
+Eos;g;Aurore, aube;Dawn
+Hespera;g;Étoile du soir;Evening star
+Lyra;g;Lyre, harmonie;Lyre, harmony
+Maia;g;Mère, déesse du printemps;Mother, spring goddess
+Oceane;g;Océan;Ocean
+Phoebe;g;Brillante, lune;Bright, moon
+Selena;g;Lune;Moon
+Theia;g;Déesse de la lumière;Goddess of light
+Thalassia;g;De la mer;Of the sea
+`};
