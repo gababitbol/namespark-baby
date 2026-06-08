44c56e64799a10bc4027f5466f916a4e83aecf47
Gabriel Abitbol <gabrielabitbol@MacBook-Air.local>
2026-06-08 15:16:16 +0300

    Palier 4000 depasse → 4008 prenoms (vertus)

---
+++ tools/bulk/divers-vertus.js
@@ -0,0 +1,17 @@
+module.exports = { origin: "anglais", data: `
+Amity;g;Amitié, concorde;Friendship
+Blythe;g;Joyeuse, insouciante;Joyful, carefree
+Constance;g;Constance, fidélité;Constancy
+Felicity;g;Bonheur, félicité;Happiness
+Justice;m;Justice, équité;Justice
+Mercy;g;Miséricorde;Mercy
+Patience;g;Patience;Patience
+Prosper;b;Prospère;Prosperous
+Valor;b;Vaillance, courage;Valour
+Verity;g;Vérité;Truth
+Cherish;g;Chérir, aimer;To cherish
+Noble;b;Noble, digne;Noble
+Haven;m;Refuge, havre;Haven
+Bliss;g;Félicité;Bliss
+Loyal;b;Loyal, fidèle;Loyal
+`};
