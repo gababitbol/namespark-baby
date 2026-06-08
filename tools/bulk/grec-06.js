43c965846d76339bd5784254641e49a95ac4ab80
Gabriel Abitbol <gabrielabitbol@MacBook-Air.local>
2026-06-08 15:15:57 +0300

    Palier 4000 franchi → 3997 prénoms

---
+++ tools/bulk/grec-06.js
@@ -0,0 +1,42 @@
+module.exports = { origin: "grec", data: `
+Akakios;b;Innocent, sans malice;Innocent
+Aristarchos;b;Meilleur chef;Best ruler
+Demetrios;b;De Déméter;Of Demeter
+Eleftherios;b;Libre, libérateur;Free, liberator
+Epaminondas;b;Supérieur en bonté;Excelling in good
+Gerasimos;b;Honoré, vénérable;Honoured
+Iakovos;b;Le supplantant;Supplanter
+Kallinikos;b;Beau vainqueur;Fair victor
+Lambros;b;Brillant, radieux;Bright, radiant
+Miltiades;b;Aux cheveux roux;Red-haired
+Nikolaos;b;Victoire du peuple;Victory of the people
+Orpheus;b;Belle voix, chant;Beautiful voice
+Pericles;b;Entouré de gloire;Surrounded by glory
+Sotirios;b;Sauveur;Saviour
+Stelios;b;Pilier, colonne;Pillar
+Themistocles;b;Gloire de la loi;Glory of law
+Vasileios;b;Roi;King
+Xenophon;b;Voix étrangère;Foreign voice
+Zacharias;b;Dieu se souvient;God remembers
+Agapios;b;Aimé, cher;Beloved
+Aikaterini;g;Pure;Pure
+Ariadni;g;Très pure;Most pure
+Chrysanthi;g;Fleur d'or;Golden flower
+Despoina;g;Dame, maîtresse;Lady
+Efthymia;g;Bonne humeur, joie;Good cheer
+Eleftheria;g;Liberté;Freedom
+Evdokia;g;Bonne volonté;Good will
+Fotini;g;Lumineuse;Luminous
+Iolanthe;g;Fleur violette;Violet flower
+Kalliroi;g;Beau ruisseau;Fair stream
+Kleio;g;Gloire, célébrer;Glory
+Magdalini;g;De Magdala;From Magdala
+Melaina;g;Sombre, profonde;Dark
+Nereida;g;Nymphe de la mer;Sea nymph
+Pelagia;g;De la haute mer;Of the sea
+Stamatina;g;Constante, stable;Steadfast
+Theodosia;g;Don de Dieu;Gift of God
+Vasiliki;g;Royale;Royal
+Xanthippe;g;Cheval blond;Golden horse
+Zoi;g;Vie;Life
+`};
